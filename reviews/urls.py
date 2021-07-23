c13af861e25597b1464f5c38b465003d7f46ea39
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-23 15:16:06 +0000

    added different view class

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ReviewView.as_view()), 
+    path("", views.ReviewView.as_view()),
+    path("thank_you", views.ThankyouView.as_view()),
+    path("reviews_list", views.ReviewListView.as_view()),
+    path("reviews_list/<int:id>", views.ReviewDetailView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,13 @@
+from typing import Any
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .form import ReviewForm
 from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
+
+from .form import ReviewForm
+from .models import Reviews
 
 # Create your views here.
 
@@ -17,12 +24,54 @@ class ReviewView(View):
     def post(self, request):
         form = ReviewForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            name = data["username"]
             form.save()
-            return render(request, "reviews/thank_you.html", {
-                "name": name
-            })
+            return HttpResponseRedirect("/thank_you")
         return render(request, "reviews/reviews.html", {
             "form": form
         })
+
+
+# Templates rendering way 1
+
+# class ThankyouView(View):
+
+#     def get(self, request):
+#         return render(request, "reviews/thank_you.html")
+
+# Templates rendering way 2 : template rendering
+
+class ThankyouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works"
+        return context
+
+
+# format to use list views
+class ReviewListView(ListView):
+    template_name = "reviews/reviews_list.html"
+    model = Reviews
+    # if object name is not define we need to use object_list in templates
+    context_object_name = "reviews"
+
+    # Extra: if we want to render a review greater than 4 we can use get queryset method
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gte=4)
+        return data
+
+# METHOD 1: Using template view
+class ReviewDetailView(TemplateView):
+    template_name = "reviews/review_detail.html"
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        review = Reviews.objects.get(id=review_id)
+        context["review"] = review
+        return context
+
+
+
