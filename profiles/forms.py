38a56dcb7a6d01fc86d44f4bd8b012bcb8336b66
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-24 10:03:27 +0000

    file upload using CreateView

--- profiles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ProfileForm(forms.Form):
-    user_image = forms.ImageField()
-    # to use ImageField we need to install pillow
-    # use command python3 -m pip install Pillow
-    
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
-from .forms import ProfileForm
+# from .forms import ProfileForm
 from .models import UserProfile
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 
@@ -12,21 +13,30 @@ from .models import UserProfile
 #             dest.write(chunk)
 
 
-# method 1
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+# # method 1
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
+
+
+# django alternate method : Using CreateView
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
